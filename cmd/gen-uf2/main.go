7ac58d26f90560d1a385082056b3c703c252bf98
Scott Feldman <sfeldma@gmail.com>
2024-03-01 11:21:56 -0800

    go generate uf2

--- cmd/gen-uf2/main.go
+++ cmd/gen-uf2/main.go
@@ -7,9 +7,10 @@ import (
 	"github.com/merliot/hp2430n"
 )
 
+//go:generate go run main.go
 func main() {
 	hp2430n := hp2430n.New("proto", "hp2430n", "proto").(*hp2430n.Hp2430n)
-	if err := hp2430n.GenerateUf2s(); err != nil {
+	if err := hp2430n.GenerateUf2s("../.."); err != nil {
 		log.Println("Error generating UF2s:", err)
 		os.Exit(1)
 	}
